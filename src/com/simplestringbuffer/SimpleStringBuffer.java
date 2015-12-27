7bb8cec42f2500f31dde28d15cbdcd2c81dc4bf0
Abhishek S P <abhisheksp1993@gmail.com>
2015-12-27 19:07:11 +0530

    Added spec to check empty StringBuffer

--- src/com/simplestringbuffer/SimpleStringBuffer.java
+++ src/com/simplestringbuffer/SimpleStringBuffer.java
@@ -1,4 +1,9 @@
 package com.simplestringbuffer;
 
 public class SimpleStringBuffer {
+
+    @Override
+    public String toString() {
+        return "";
+    }
 }
--- test/com/simplestringbuffer/SimpleStringBufferTest.java
+++ test/com/simplestringbuffer/SimpleStringBufferTest.java
@@ -1,5 +1,16 @@
 package com.simplestringbuffer;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class SimpleStringBufferTest {
+
+    @Test
+    public void shouldReturnEmptyStringWhenToStringIsCalledForEMptyStringBuffer() {
+        SimpleStringBuffer simpleStringBuffer = new SimpleStringBuffer();
+
+        assertEquals("", simpleStringBuffer.toString());
+    }
 
 }
